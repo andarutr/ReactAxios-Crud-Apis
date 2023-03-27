f4a9d88e657b72608d90a9d0bc81e4e6b7c6217b
Andaru Triadi <54014525+andarutr@users.noreply.github.com>
2023-03-27 23:59:14 +0700

    Update & Delete

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
       members: [],
       first_name: "",
       last_name: "",
-      buttonDisabled: false
+      buttonDisabled: false,
+      formStatus: "create",
+      memberIdSelected: null
     };
   }
 
@@ -36,15 +38,72 @@ class App extends Component {
       first_name: this.state.first_name,
       last_name: this.state.last_name
     }
-    let url = 'https://reqres.in/api/users';
+    let url = '';
+    if(this.state.formStatus === "create")
+    {
+      url = 'https://reqres.in/api/users';
+      this.addMember(url, payload);
+    }else{
+      url = 'https://reqres.in/api/users/${this.state.memberIdSelected}';
+      this.editMember(url, payload);
+    }
+  }
+
+  addMember = (url, payload) => {
     axios.post(url, payload)
+    .then(response => {
+      console.log(response);
+      let members = [...this.state.members];
+      members.push(response.data);
+      this.setState({members, buttonDisabled: false, first_name: "", last_name: ""});
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  editMember = (url, payload) => {
+    axios.put(url, payload)
       .then(response => {
-        console.log(response);
         let members = [...this.state.members];
-        members.push(response.data);
-        this.setState({members, buttonDisabled: false, first_name: "", last_name: ""});
-      })
-      .catch(error => {
+        let indexmember = members.findIndex(
+          member => member.id === this.state.memberIdSelected
+        );
+        members[indexmember].first_name = response.data.first_name;
+        members[indexmember].last_name = response.data.last_name;
+        
+        this.setState({
+          members,
+          buttonDisabled: false,
+          first_name: "",
+          last_name: "",
+          formStatus: "create",
+        });
+      }).catch(error => {
+        console.log(error);
+      });
+  }
+
+  editButtonHandler = (member) => {
+    this.setState({
+      first_name: member.first_name,
+      last_name: member.last_name,
+      formStatus: "edit",
+      memberIdSelected: member.id
+    });
+  }
+
+  deleteButtonHandler = id => {
+    let url = 'https://reqres.in/api/users/${id}';
+    axios.delete(url)
+      .then(response => {
+        if(response.status === 204){
+          let members = [...this.state.members];
+          let index = members.findIndex(member => member.id === id);
+          members.splice(index, 1);
+          this.setState({members});
+        }
+      }).catch(error => {
         console.log(error);
       });
   }
@@ -66,8 +125,8 @@ class App extends Component {
                           <h5 className="card-title">{member.id}</h5>
                           <h5 className="card-title">{member.first_name}</h5>
                           <h5 className="card-title">{member.last_name}</h5>
-                          <button className="btn btn-primary">Edit</button>
-                          <button className="btn btn-danger">Delete</button>
+                          <button className="btn btn-primary" onClick={() => this.editButtonHandler(member)}>Edit</button>
+                          <button className="btn btn-danger" onClick={() => this.deleteButtonHandler(member.id)}>Delete</button>
                       </div>
                   </div>
                 </div>
@@ -75,7 +134,7 @@ class App extends Component {
               </div>
             </div>
             <div className="col-md-6" style={{ border: "1px solid black" }}>
-              <h2>Form</h2>
+              <h2>Form {this.state.formStatus}</h2>
               <form onSubmit={this.onSubmitHandler}>
                 <div className="form-group">
                 <label>First Name</label>
