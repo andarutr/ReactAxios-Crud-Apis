d68af47de24360ed2f95eb3857b25cc294c6789a
Andaru Triadi <54014525+andarutr@users.noreply.github.com>
2023-03-27 16:15:54 +0700

    First Push

---
+++ src/component/Navbar.js
@@ -0,0 +1,16 @@
+function Navbar(){
+    return(
+        <div>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/768px-LEGO_logo.svg.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
+                    &nbsp; Latihan Axios
+                    </a>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
